dfe48b897a461cbd8718a4ba847e3b80462f9c0e
gweo <hallbodie1@gmail.com>
2026-09-17 17:35:01 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import Packs from './listpacks.js'; // 1. Add this import line at the top
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/packs', component: Packs }, // 2. Add this route entry at the bottom
 ];
